6e0ca023d752e02670b373a658ded06b31e1ee10
Aditya Guha <119130187+hermit571@users.noreply.github.com>
2023-07-07 02:19:23 +0530

    Add files via upload

---
+++ Random Password Generator.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length, include_digits=True, include_punctuation=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    while True:
+        length = int(input("Enter the length of your Password: "))
+        password = generate_password(length, include_digits=True, include_punctuation=True)
+        print("Generated Password:", password)
+
+        choice = input("Generate another password? (Y/N): ")
+        if choice.lower() != "y":
+            break
